89e61b33036f28374e72689225c84d3563e5de45
Yaniv Pascal <yanivpas@gmail.com>
2015-10-14 21:17:32 +0300

    strating the space module

--- space.go
+++ space.go
@@ -1,11 +1,30 @@
 package space
 
 import (
+	"container/list"
 	"fmt"
 	"math"
 )
 
-func main() {
-	fmt.Println("hello world")
+type Space struct {
+	vectors *List
+}
+
+const g_dimention = 2
+const g_width = 400
+const g_hight = 400
+
+func (s *Space) AddVector(v Vector) {
+
+}
+
+func (s *Space) Init() {
+	vectors := list.New()
+	s.vectors = vectors
+	return s
+}
+func New() *Space {
+	fmt.Println("creating a world")
 	fmt.Println(math.Pi)
+	return new(Space).Init()
 }
